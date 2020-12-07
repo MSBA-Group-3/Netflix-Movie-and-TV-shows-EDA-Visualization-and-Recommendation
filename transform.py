cee0d63ac9b635c715ed6302ee39540ebfa4013a
sjayan <sjayan@ucsd.edu>
2020-12-07 15:44:17 -0800

    Added average vote to transformed_data_2

--- transform.py
+++ transform.py
@@ -6,33 +6,36 @@ netflix_ratings = pd.read_csv("netflix_ratings.csv")
 title = netflix_ratings.loc[:, "title"]
 year = netflix_ratings.loc[:, "year"]
 genre = netflix_ratings.loc[:, "genre"]
+avg_vote = netflix_ratings.loc[:, "avg_vote"]
 country = netflix_ratings.loc[:, "country"]
 
 
-df = pd.concat([title, year, genre, country], axis = 1)
+df = pd.concat([title, year, genre, avg_vote, country], axis = 1)
 
 
 titles = []
 years = []
 genres = []
 countries = []
+votes = []
 for index, row in df.iterrows():
 	for genre in row.genre.split(","):
 		for country in row.country.split(","):
 			titles.append(row.title)
 			years.append(row.year)
 			genres.append(genre)
+			votes.append(row.avg_vote)
 			countries.append(country)
 
 
-headerRowOne = ['title', 'year', 'genre', 'country']
+headerRowOne = ['title', 'year', 'genre', 'avg_vote', 'country']
 output_filename = 'transformed_data.csv'
 output_file = open(output_filename, 'w')
 
 writer = csv.writer(output_file)
 writer.writerow(headerRowOne)
 
-rows = zip(titles, years, genres, countries)
+rows = zip(titles, years, genres, votes, countries)
 
 for row in rows:
 	writer.writerow(row)
--- transform_large.py
+++ transform_large.py
@@ -23,32 +23,36 @@ title = dat.loc[:, "title"]
 year =dat.loc[:, "release_date"]
 genre = dat.loc[:, "genres"]
 country = dat.loc[:, "production_countries"]
+avg_vote = dat.loc[:, "weighted_average_vote"]
 
 
-df = pd.concat([title, year, genre, country], axis = 1)
+df = pd.concat([title, year, genre, avg_vote, country], axis = 1)
 
 
 titles = []
 years = []
 genres = []
 countries = []
+votes = []
+
 for index, row in df.iterrows():
 	for genre in row.genres:
 		for country in row.production_countries:
 			titles.append(row.title)
 			years.append(row.release_date)
 			genres.append(genre)
+			votes.append(row.weighted_average_vote)
 			countries.append(country)
 
 
-headerRowOne = ['title', 'year', 'genre', 'country']
+headerRowOne = ['title', 'year', 'genre', 'avg_vote', 'country']
 output_filename = 'transformed_data_2.csv'
 output_file = open(output_filename, 'w')
 
 writer = csv.writer(output_file)
 writer.writerow(headerRowOne)
 
-rows = zip(titles, years, genres, countries)
+rows = zip(titles, years, genres, votes, countries)
 
 for row in rows:
 	writer.writerow(row)
